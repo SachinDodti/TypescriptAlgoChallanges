bc187218f8435efdefc9c6f810c0cd51e0cca62b
SachinDodti <sachin.dodti99@gmail.com>
2021-06-22 11:32:15 +0530

    addBorder 2ways

--- addBorder.ts
+++ addBorder.ts
@@ -1,29 +1,36 @@
-function addBorder(picture:string[]) :any {
+function addBorder(picture: string[]): any {
+  console.log(picture);
+  let lengthOfWall = picture[0].length + 2;
+  //let lengthOfWall = '*'.repeat()
+  let wall = "";
+  for (let i = 0; i < lengthOfWall; i++) {
+    wall = wall.concat("*");
+  }
 
-    console.log(picture)
-    let lengthOfWall = picture[0].length + 2 
-    let wall = ""
-   for (let i = 0 ;i< lengthOfWall;i++){
+  console.log(wall);
 
-   wall = wall.concat("*")
+  picture.unshift(wall);
+  picture.push(wall);
 
-   }
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+    console.log(picture[i]);
+  }
 
-   console.log(wall)
-
-
-   picture.unshift(wall)
-   picture.push(wall)
-
-   for (let i=1 ; i < picture.length -1 ; i++){
-
-    picture[i] = "*".concat(picture[i],"*");
-    console.log(picture[i])
-
-   }
-   
-   return picture
+  return picture;
 }
 
+function addBorder2(picture: string[]): any {
+  let lengthOfWall = "*".repeat(picture[0].length + 2);
 
-console.log(addBorder(['abc','def']))
+  picture.unshift(lengthOfWall);
+  picture.push(lengthOfWall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+  }
+
+  return picture;
+}
+//console.log(addBorder(["abc", "def"]));
+console.log(addBorder2(["abc", "def"]));
